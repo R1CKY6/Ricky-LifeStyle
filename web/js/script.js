90689156b9a6e4a5a375aa8bcaa7186e4670e311
R1CKY6 <100082886+R1CKY6@users.noreply.github.com>
2023-07-16 11:36:00 +0200

    Update script.js

--- web/js/script.js
+++ web/js/script.js
@@ -3,120 +3,9 @@ const app = new Vue({
     data: {
         nomeRisorsa : GetParentResourceName(),
 
-        listaStili : [
-            {
-                nome: "IMPRENDITORE",
-                id : 'imprenditore',
-                image : 'imprenditore.png',
-                descrizione:  "Sfrutta ogni opportunità di mercato per creare e gestire aziende di successo, persegui i tuoi obiettivi.",
-                oggetti : [
-                    {
-                        label : "Telefono",
-                        quantity : 1,
-                        name : 'telefono'
-                    },
-                    {
-                        label : "Panini",
-                        quantity : 3,
-                        name : 'pane'
-                    },
-                    {
-                        label : "Acqua",
-                        quantity : 2,
-                        name : 'acqua'
-                    },
-                    {
-                        label : "Contanti",
-                        quantity: 5000,
-                        name : 'money'
-                    }
-                ]
-            },
-            {
-                nome: "CRIMINALE",
-                id: 'criminale',
-                image : 'criminale.png',
-                descrizione:  "Fatti coinvolgere in attività illegali, fai rapine, traffica droga e guadagna più soldi possibili.",
-                oggetti : [
-                    {
-                        label : "Grimaldello",
-                        quantity : 1,
-                        name : 'grimaldello'
-                    },
-                    {
-                        label : "Panini",
-                        quantity : 3,
-                        name : 'pane'
-                    },
-                    {
-                        label : "Acqua",
-                        quantity : 2,
-                        name : 'acqua'
-                    },
-                    {
-                        label : "Contanti",
-                        quantity: 1000,
-                        name : 'money'
-                    }
-                ]
-            },
-            {
-                nome: "FORZA DELL'ORDINE",
-                id : 'fdo',
-                image : 'fdo.png',
-                descrizione:  "Applica la legge e mantieni l’ordine nell’interà città",
-                oggetti : [
-                    {
-                        label : "Telefono",
-                        quantity : 1,
-                        name : 'telefono'
-                    },
-                    {
-                        label : "Panini",
-                        quantity : 3,
-                        name : 'pane'
-                    },
-                    {
-                        label : "Acqua",
-                        quantity : 2,
-                        name : 'acqua'
-                    },
-                    {
-                        label : "Contanti",
-                        quantity: 4000,
-                        name : 'money'
-                    }
-                ]
-            },
-            {
-                nome: "CIVILE",
-                id : 'civile',
-                image : 'civile.png',
-                descrizione:  "Non farti coinvolgere nei giri dei criminali e dei poliziotti, stai nel tuo e guadagnati soldi facendo lavori onesti come il macellaio.",
-                oggetti : [
-                    {
-                        label : "Telefono",
-                        quantity : 1,
-                        name : 'telefono'
-                    },
-                    {
-                        label : "Panini",
-                        quantity : 3,
-                        name : 'pane'
-                    },
-                    {
-                        label : "Acqua",
-                        quantity : 2,
-                        name : 'acqua'
-                    },
-                    {
-                        label : "Contanti",
-                        quantity: 2000,
-                        name : 'money'
-                    }
-                ]
-            },
-        ]
+        config : [],
+
+        listaStili : []
     },
 
     methods: {
@@ -145,5 +34,8 @@ window.addEventListener('message', function(event) {
     var data = event.data;
     if (data.type === "OPEN") {
         $("#app").fadeIn(500);
+    }else if(data.type === "SET_CONFIG") {
+        app.config = data.config;
+        app.listaStili = data.config.Stili;
     }
 })
